ed63fafa41ec0df3a9337c92f98ad33a5fa351c5
chris27 <christian.leovido@gmail.com>
2017-07-27 22:06:06 +0100

    add nodemailer to contact form / remove keys section / add survey

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express(),
+    bodyParser = require('body-parser'),
+    nodemailer = require('nodemailer'),
     router = express.Router({mergeParams: true})
-
 
 router.get('/', function(req, res, next) {
     res.render('index')
@@ -9,6 +10,57 @@ router.get('/', function(req, res, next) {
 
 router.get('/posture', function(req, res, next) {
     res.render('posture')
+});
+
+router.post('/contribute', function (req, res) {
+
+    // Create a SMTP transport object
+    var transport = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: "xtian993@gmail.com",
+                pass: "process.env.SECRET"
+            }
+        });
+
+    console.log('SMTP Configured');
+
+    // Message object
+    var message = {
+
+        // sender info
+        from: 'Keys to Teach',
+
+        // Comma separated list of recipients
+        to: ['marina.g16@edu.trinitylaban.ac.uk'],
+
+        // Subject of the message
+        subject: 'New contribution from Keys to Teach', 
+
+        // plaintext body
+        text: 'Message: ' + req.body.message + '\n' + 'Name: ' + req.body.name + '\n' + 'Email: ' + req.body.email,
+
+        // HTML body
+        html:'Message: ' + req.body.message + '\n' + 'Name: ' + req.body.name + '\n' + 'Email: ' + req.body.email,
+    
+    };
+
+    console.log('Sending Mail');
+    transport.sendMail(message, function(error){
+    if(error){
+        console.log(error);
+            req.flash('error', "Email or message required");
+            res.redirect('/');
+        return;
+    }
+        console.log('Message sent!');
+            req.flash('success', 'Thank you for your contribution');
+            res.redirect('/');
+
+    // if you don't want to use this transport object anymore, uncomment following line
+    //transport.close(); // close the connection pool
+    });
+            
 });
 
 module.exports = router;
