08c42f5b89a3cec874c6c69c40d85f9ef8d55594
Christian Ray Leovido <christian.leovido@gmail.com>
2017-08-16 18:08:33 +0100

    update jss and css

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,22 @@ module.exports = function(grunt) {
     grunt.initConfig({
         concat: {
           js: {
-            src: ['./public/js/agency.min.js', './public/js/bootstrap.min.js', './public/js/jquery.min.js'],
-            dest: 'dist/js/scripts.js',
+            src: ['./public/js/jquery.min.js', './public/js/bootstrap.min.js'],
+            dest: 'public/dist/js/scripts.js',
           },
           css: {
-            src: ['./public/stylesheets/bootstrap/css/main.min.css', './public/stylesheets/bootstrap/css/bootstrap.min.css', './public/stylesheets/bootstrap/font-awesome/css/font-awesome.min.css'],
-            dest: 'dist/css/styles.css',
+            src: ['./public/stylesheets/bootstrap/css/main.min.css', './public/stylesheets/bootstrap/font-awesome/css/font-awesome.min.css'],
+            dest: 'public/dist/css/styles.css',
           }
         },
         watch: {
           js: {
             files: ['./public/js/**/*.js'],
             tasks: ['concat:js', 'uglify']
+          },
+          css: {
+            files: ['./public/css/**/*.css'],
+            tasks: ['concat:css', 'cssmin']
           }
         },
         cssmin: {
@@ -24,14 +28,14 @@ module.exports = function(grunt) {
           },
           target: {
             files: {
-              'dist/css/main.min.css': ['dist/css/styles.css']
+              'public/dist/css/main.min.css': ['public/dist/css/styles.css']
             }
           }
         },
         uglify: {
           my_target: {
             files: {
-              'dist/output.min.js': ['dist/js/scripts.js']
+              'public/dist/js/scripts.min.js': ['public/dist/js/scripts.js']
             }
           }
         }
