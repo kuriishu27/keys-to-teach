bff9422236b8d47acaffce5a6ae4de7f27cbda06
Christian Ray Leovido <christian.leovido@gmail.com>
2017-08-30 16:20:52 +0100

    switch position for methodOverride

--- app.js
+++ app.js
@@ -28,10 +28,11 @@ mongoose.connect(url);
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(methodOverride('_method'));
+
 app.use(require('express-session')({
     secret: 'Keys to teach session',
     resave: false,
